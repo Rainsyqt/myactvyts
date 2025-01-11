e7d9fa52a3823e3f393ade299b0d31e67f2500c8
Andreigh Espino <andreighespino@yahoo.com>
2025-01-11 09:01:40 +0800

    Add files via upload

---
+++ mybio.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+ using namespace std;
+
+  int main(){
+
+    string name = "Andreigh";
+    string last = "Espino";
+    int age = 20;
+    bool boolvalue = true;
+    double height = 174.5;
+    float weight = 70;
+    
+    cout << "Name: " << name << endl;
+    cout << "LastName: " << last << endl;
+    cout << "Age:" << age << endl;
+    cout << "Male:" << boolvalue << endl;
+    cout << "Height(cm): " << height << endl;
+    cout << "Weight(kg): " << weight << endl;
+    
+    
+    return 0;
+}
